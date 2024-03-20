9a7f209512b82d83a05f9e71b8b4c2e9de2cfd02
Greg Medding <gregory.medding@gm.com>
2024-03-19 19:26:37 -0700

    Refactor for Singleton namespace, handle checking more gracefully

--- include/Singleton.h
+++ include/Singleton.h
@@ -8,13 +8,18 @@
 #include <utility>
 #include <variant>
 
-struct SingletonCheck {
+namespace Singleton {
+
+struct Check {
     virtual bool instanceOk() const = 0;
+};
+
+struct Result {
     virtual std::any instanceResult() const { return {}; };
 };
 
 template<typename ResultT>
-struct SingletonCheckResult : public SingletonCheck {
+struct CheckWithResult : public Check, public Result {
     static ResultT const getResultValue(std::any const& result) {
         return std::any_cast<ResultT>(result);
     }
@@ -26,12 +31,12 @@ private:
 };
 
 template<typename T>
-class Singleton : public T {
+class Wrapper : public T {
     template<typename... Args>
-    Singleton(Args&&... args) : T(std::forward<Args>(args)...) { }
+    Wrapper(Args&&... args) : T(std::forward<Args>(args)...) { }
 
-    Singleton(Singleton const&) = delete;
-    Singleton& operator=(Singleton const&) = delete;
+    Wrapper(Wrapper const&) = delete;
+    Wrapper& operator=(Wrapper const&) = delete;
 
     static inline std::weak_ptr<T> weak_instance_handle_;
     static inline std::mutex instance_lock_;
@@ -39,7 +44,6 @@ class Singleton : public T {
 
     template<typename... Args>
     static std::pair<std::shared_ptr<T>, MaybeLock> instance_impl(Args&&... args) {
-
         if (auto instance_handle = weak_instance_handle_.lock()) {
             return {instance_handle, MaybeLock()};
         } else {
@@ -60,8 +64,9 @@ public:
     using WrapT = T;
     using Handle = std::shared_ptr<T>;
 
-    template<typename... Args, typename T2 = T, std::enable_if_t<!std::is_base_of_v<SingletonCheck, T2>, bool> = true>
-    [[nodiscard]] static std::shared_ptr<T> instance(Args&&... args) {
+    template<typename... Args, typename T2 = T,
+        std::enable_if_t<!std::is_base_of_v<Check, T2>, bool> = true>
+    [[nodiscard]] static Handle instance(Args&&... args) {
         auto instance_and_lock = instance_impl(std::forward<Args>(args)...);
         auto& instance_handle{instance_and_lock.first};
         auto& maybe_lock{instance_and_lock.second};
@@ -74,19 +79,52 @@ public:
         return instance_handle;
     }
 
-    template<typename... Args, typename T2 = T, std::enable_if_t<std::is_base_of_v<SingletonCheck, T2>, bool> = true>
-    [[nodiscard]] static std::variant<std::shared_ptr<T>, std::any> instance(Args&&... args) {
+    template<typename... Args, typename T2 = T,
+        std::enable_if_t<
+            std::is_base_of_v<Check, T2> && !std::is_base_of_v<Result, T2>,
+            bool> = true>
+    [[nodiscard]] static Handle instance(Args&&... args) {
         auto instance_and_lock = instance_impl(std::forward<Args>(args)...);
         auto& instance_handle{instance_and_lock.first};
         auto& maybe_lock{instance_and_lock.second};
-        
+
         // If a lock was handed to us, that means a new instance was created
         if (maybe_lock) {
             if (instance_handle) {
-                if (auto& check = *dynamic_cast<SingletonCheck*>(instance_handle.get()); check.instanceOk()) {
+                auto check = dynamic_cast<Check*>(instance_handle.get());
+                if (check && check->instanceOk()) {
                     weak_instance_handle_ = instance_handle;
                 } else {
-                    return check.instanceResult();
+                    return {};
+                }
+            }
+        }
+
+        return instance_handle;
+    }
+
+    template<typename... Args, typename T2 = T,
+        std::enable_if_t<
+            std::is_base_of_v<Check, T2> && std::is_base_of_v<Result, T2>,
+            bool> = true>
+    [[nodiscard]] static std::variant<Handle, std::any> instance(Args&&... args) {
+        auto instance_and_lock = instance_impl(std::forward<Args>(args)...);
+        auto& instance_handle{instance_and_lock.first};
+        auto& maybe_lock{instance_and_lock.second};
+
+        // If a lock was handed to us, that means a new instance was created
+        if (maybe_lock) {
+            if (instance_handle) {
+                auto check = dynamic_cast<Check*>(instance_handle.get());
+                if (check && check->instanceOk()) {
+                    weak_instance_handle_ = instance_handle;
+                } else {
+                    auto result = dynamic_cast<Result*>(instance_handle.get());
+                    if (result) {
+                        return result->instanceResult();
+                    } else {
+                        return {};
+                    }
                 }
             }
         }
@@ -94,5 +132,7 @@ public:
         return instance_handle;
     }
 };
+
+} // namespace Singleton
 
 #endif // SINGLETON_H
